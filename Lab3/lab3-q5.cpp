3a1a2007792f223615920532341e8c6fbb66d3c3
JamesMainPC <heringja@miamioh.edu>
2022-09-02 14:43:40 -0400

    Made things more organized and might've gotten the coding part done for Lab3

--- Lab3/lab3-q5.cpp
+++ Lab3/lab3-q5.cpp
@@ -1,20 +1,13 @@
 // Copyright (c) James Hering 9/2/2022
 #include <iostream>
 // #include <bitset>
-
-int main()
-{
+int main() {
     u_int16_t number;
+    std::cout << "Input a number\n";
     std::cin >> number;
     u_int16_t mask = 1 << 1;
     u_int16_t result = number & mask;
-    if (result == 0)
-    {
-        std::cout << "Second least significant bit is 0";
-    }
-    else
-    {
-        std::cout << "Second least significant bit is 1";
-    }
+    if (result == 0) {std::cout << "Second least significant bit is 0\n";}
+    else {std::cout << "Second least significant bit is 1\n";}
     return 0;
 }
