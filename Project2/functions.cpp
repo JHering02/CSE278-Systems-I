b3c96daced437bcb9c0a57b80ca38ac6df44510a
James <heringja@miamioh.edu>
2022-10-14 18:36:39 -0400

    Progress on Project 2

--- Project2/functions.cpp
+++ Project2/functions.cpp
@@ -7,13 +7,46 @@
 #include <iostream>
 #include <fstream> 
 
-void listNames(std::string fileName){
+std::ifstream openFile(std::string fileName) {
     std::ifstream inFile;
     inFile.open(fileName);
     if (!inFile.is_open()) {
       std::cout << "!!! Could not open the input file(s)" << std::endl;
       return;
-   } 
+   } else {
+        return inFile;
+   }
+}
+
+void listCrewFlights(std::string fileName) {
+    bool contained = false;
+    std::ifstream inFile = openFile(fileName);
+    std::string crew, schedule;
+    std::cout << ">> Enter the name of the crew :";
+    std::cin >> crew;
+    for(char ch : crew) { toupper(ch); }
+    while(!(inFile.eof())) {
+        std::string name, temp;
+        inFile >> name;
+        for(char ch : name) { toupper(ch); }
+        if(crew.compare(name) == 0) {
+            contained = true;
+            inFile >> temp;
+            // Do binary stuff to read the flights
+        }
+        std::getline(inFile, temp);
+    }
+    if ((contained) && (schedule.size() > 0)) {
+        std::cout << "---- Assigned Flights ----\n" << schedule;
+    } else if (contained && !(schedule.size() > 0)) {
+        std::cout << "No flight was assigned to " << crew << " !!!\n";
+    } else {
+        std::cout << "!!! No record for " << crew << std::endl;
+    }
+    inFile.close();
+}
+void listNames(std::string fileName){
+   std::ifstream inFile = openFile(fileName);
    std::cout << "--- Names of The Crews ---" << std::endl;
     while(!(inFile.eof())) {
         std::string name, temp;
@@ -21,12 +54,13 @@ void listNames(std::string fileName){
         std::cout << name << std::endl;
         std::getline(inFile, temp);
     }
+    inFile.close();
 }
 
 void respond(int& response) {
     switch(response) {
         case(1) :
-            std::cout << "List assigned flights of a crew\n";
+            listCrewFlights("CrewSchedule.txt");
             break;
         case(2) :
             std::cout << "List crews for a flight\n";
--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,20 +1,23 @@
 #include "functions.h"
 #include <iostream>
+#include <iomanip>
 #include <fstream> 
 #include <limits>
 
-
-// Menu Components
-const std::string menu = "-----------------------------------\n"
-                        "Airline Crew Scheduling Application\n"
-                        "-----------------------------------\n"
-                        " 1 List assigned flights of a crew\n"
-                        " 2 List crews for a flight\n"
-                        " 3 List names of the crews\n"
-                        " 4 [Your Option]\n"
-                        "-1 Exit\n"
-                        "-----------------------------------\n";
+void printMenu() {
+    // Menu Components
+    std::cout << std::setfill('-') << std::setw(35) << std::endl;
+    std::cout << "Airline Crew Scheduling Application" << std::endl;
+    std::cout << std::setfill('-') << std::setw(35) << std::endl;
+    std::cout << " 1 List assigned flights of a crew\n";
+    std::cout << " 2 List crews for a flight\n";
+    std::cout << " 3 List names of the crews\n";
+    std::cout << " 4 [Your Option]\n";
+    std::cout << "-1 Exit\n";
+    std::cout << std::setfill('-') << std::setw(35) << std::endl;
+}
 void enterToContinue() {
+    // Function to pause the menu until the user presses enter.
     std::cout << "\nto continue, press enter...";
     std::cin.ignore();
 }
@@ -27,7 +30,7 @@ int main() {
         flights.close();
         schedule.close();
         do {
-            std::cout << menu << ">> Enter your choice:" << std::endl;
+            printMenu();
             std::cin >> response;
             respond(response);
             if(response != -1) {
