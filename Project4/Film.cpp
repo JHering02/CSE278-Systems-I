0b5b9ca0981e4627f08d25430230dd00fed16170
JamesMainPC <heringja@miamioh.edu>
2022-11-30 01:13:22 -0500

    Film class is generally complete, will be used to make objects during a query

--- Project4/Film.cpp
+++ Project4/Film.cpp
@@ -1,5 +1,24 @@
 #define MYSQLPP_MYSQL_HEADERS_BURIED
 
 #include "Film.h"
+#include <mysql++/mysql++.h>
 #include <string>
 #include <iostream>
+
+Film::Film(std::string ti, std::string desc, std::string rate, std::string len) {
+   title = ti;
+   description = desc;
+   rating = rate;
+   length = stoi(len);
+}
+
+void Film::addActors(mysqlpp::StoreQueryResult& myRes) {
+  for (const auto& row : myRes) {
+     std::string fName = row[0].c_str();
+     std::string lName = row[1].c_str();
+     actorNames.push_back(fName + " " + lName);
+  }
+}
+
+
+
--- Project4/Film.h
+++ Project4/Film.h
@@ -1,3 +1,21 @@
+#ifndef FILM_H
+#define FILM_H
 #define MYSQLPP_MYSQL_HEADERS_BURIED
 
 #include <mysql++/mysql++.h>
+#include <string>
+#include <iostream>
+
+class Film {
+  public :
+    Film(std::string ti, std::string desc, std::string rate, std::string len);
+    void addActors(mysqlpp::StoreQueryResult& myRes);
+  private : 
+    std::string title;
+    std::string description;
+    std::string rating;
+    int length;
+    std::vector<std::string> actorNames;
+};
+
+#endif
